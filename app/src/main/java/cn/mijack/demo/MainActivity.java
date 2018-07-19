5c39110f0c6be93ba8838eb0e2321c0772aa6c0e
Mi&Jack <mijackstudio@gmail.com>
2018-07-20 00:26:58 +0800

    修改版本

--- app/src/main/java/cn/mijack/demo/MainActivity.java
+++ app/src/main/java/cn/mijack/demo/MainActivity.java
@@ -2,23 +2,16 @@ package cn.mijack.demo;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.mijack.IStatable;
-
-import java.lang.reflect.Member;
-import java.lang.reflect.Method;
 
 import cn.Clicker;
-import cn.XlogStater;
 
 /**
  * @author Mi&Jack
  */
-public class MainActivity extends Activity implements IStatable {
+public class MainActivity extends Activity  {
     StringBuilder state = new StringBuilder();
     private Button button;
     private Button button2;
@@ -39,8 +32,4 @@ public class MainActivity extends Activity implements IStatable {
         button3.setOnClickListener(clicker);com.mijack.Xlog.logMethodExit("void cn.mijack.demo.MainActivity.onCreate(android.os.Bundle)",this);}catch(Throwable throwable){com.mijack.Xlog.logMethodExitWithThrowable("void cn.mijack.demo.MainActivity.onCreate(android.os.Bundle)",this,throwable);throw throwable;}
     }
 
-    @Override
-    public String getState() {
-        com.mijack.Xlog.logMethodEnter("java.lang.String cn.mijack.demo.MainActivity.getState()",this);try{com.mijack.Xlog.logMethodExit("java.lang.String cn.mijack.demo.MainActivity.getState()",this);return state.toString();}catch(Throwable throwable){com.mijack.Xlog.logMethodExitWithThrowable("java.lang.String cn.mijack.demo.MainActivity.getState()",this,throwable);throw throwable;}
-    }
 }
